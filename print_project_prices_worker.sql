42f0ab5cc5ee476d4808b816603b5dffcdc98186
Raisa <128744721+Raya97@users.noreply.github.com>
2023-11-13 07:58:45 +0100

    Delete print_project_prices_worker.sql

--- print_project_prices_worker.sql
+++
@@ -1,9 +0,0 @@
--- Знайдемо вартість кожного проєкту
-SELECT p.ID AS PROJECT_ID,
-       SUM(w.SALARY) * TIMESTAMPDIFF(MONTH, p.START_DATE, p.FINISH_DATE) AS PRICE
-FROM project p
-JOIN project_worker pw ON p.ID = pw.PROJECT_ID
-JOIN worker w ON pw.WORKER_ID = w.ID
-GROUP BY p.ID
-ORDER BY PRICE DESC;
-
